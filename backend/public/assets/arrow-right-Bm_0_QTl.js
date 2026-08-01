08590706dd3851da6533070be176a6ffa1ec80bb
abhinavagxprience <abhinavagxprience@gmail.com>
2026-08-02 00:16:19 +0530

    build: update static frontend build and backend public assets

--- backend/public/assets/arrow-right-Bm_0_QTl.js
+++
@@ -1,1 +0,0 @@
-import{v as r}from"./index-TEdVOo0y.js";const o=r("ArrowRight",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]]);export{o as A};
--- backend/public/assets/more-horizontal-KkctSOkz.js
+++
@@ -1,1 +0,0 @@
-import{v as c}from"./index-TEdVOo0y.js";const e=c("MoreHorizontal",[["circle",{cx:"12",cy:"12",r:"1",key:"41hilf"}],["circle",{cx:"19",cy:"12",r:"1",key:"1wjl8i"}],["circle",{cx:"5",cy:"12",r:"1",key:"1pcz8c"}]]);export{e as M};
