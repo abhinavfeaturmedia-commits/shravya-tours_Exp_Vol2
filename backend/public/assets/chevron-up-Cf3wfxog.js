13c16aaeaca7c1ca24a4cfbd7909942e6f20e86d
abhinavagxprience <abhinavagxprience@gmail.com>
2026-08-09 23:07:27 +0530

    build: update production static assets in backend/public

--- backend/public/assets/chevron-up-Cf3wfxog.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./index-zBSvS7Yc.js";const r=e("ChevronUp",[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]]);export{r as C};
---
+++ backend/public/assets/loader-2-D-ieFpbJ.js
@@ -0,0 +1,1 @@
+import{w as a}from"./index-CKBYaTrL.js";const o=a("Loader2",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]);export{o as L};
