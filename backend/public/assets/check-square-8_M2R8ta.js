840d7684dca75ad982b4f47709a0a64351f666cb
abhinavagxprience <abhinavagxprience@gmail.com>
2026-08-28 23:21:54 +0530

    build(dist): update production bundle in backend/public for Hostinger deployment

---
+++ backend/public/assets/check-square-8_M2R8ta.js
@@ -0,0 +1,1 @@
+import{w as e}from"./index-CYIL8YUm.js";const o=e("CheckSquare",[["polyline",{points:"9 11 12 14 22 4",key:"19ybtz"}],["path",{d:"M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11",key:"1jnkn4"}]]);export{o as C};
